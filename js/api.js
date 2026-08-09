b8a37cdfa8ff3eaa002e3fe06db456f7a9e44def
batuparadisefactoryoutlet1688 <batuparadisefactoryoutlet1688@gmail.com>
2026-08-09 14:30:04 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@
    ============================================================ */
 
 // GANTI dengan URL deploy Apps Script kamu (Deploy > Manage deployments)
-const API_URL = 'https://script.google.com/macros/s/AKfycbymHDbwb6wTdH_IPVA5A9vyxi-M_22LOy5nD6NrnIvGqrQ_ZkSbkVIyQGR3yzaPzRTR/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxGcgrCR6nz1Ne_U-8dlPNpOG-m01CAR5DWU4xxxmdMx0-mQMIgAgjoD4jiK_tTTYgJ/exec';
 
 const Api = {
   async getAll() {
